527f477c349f8144451d02a886575492999334fe
Iago Ramos <iagofrontend@gmail.com>
2020-01-26 11:30:59 -0300

    Estrutura Site

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+/*alert('Olá mundo');*/
+
+/*
+
+document.getElementById("enviar").onclick = function () {
+
+  var n1 = document.getElementById("name1").value;
+  var n2 = document.getElementById("name2").value;
+  var n3 = parseInt(n1) + parseInt(n2);
+  var a = document.getElementById("resultado")
+
+  a.innerHTML = n3
+  alert(n3)
+
+  console.log(document.getElementById("resultado").value)
+}
+
+*/
